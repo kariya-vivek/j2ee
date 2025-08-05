3c822cc18901b5998a0350e9dc69019a26d38fde
kariya-vivek <vivekkariya22@gmail.com>
2025-08-05 08:37:03 +0530

    Update practical26sev.java

--- insandsel.java
+++ insandsel.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ practical26
  */
 
 import java.io.IOException;
@@ -105,3 +106,4 @@ public class insandsel extends HttpServlet {
     }// </editor-fold>
 
 }
+
